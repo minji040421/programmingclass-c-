184f450160c7ca6c6fe4bea9673328410d27297f
minji040421 <minji12321@naver.com>
2025-03-26 23:20:28 +0900

    Update 9번 문제.cpp

--- chapter3 the last/chapter3/250320 객프 2024003033 김민지 과제/250320 객프 2024003033 김민지 과제/9번 문제.cpp
+++ chapter3 the last/chapter3/250320 객프 2024003033 김민지 과제/250320 객프 2024003033 김민지 과제/9번 문제.cpp
@@ -4,43 +4,57 @@ using namespace std;
 #include<cstdlib>
 #include<ctime>
 
-//9¹ø ¹®Á¦
-/*
+//9ë²ˆ ë¬¸ì œ
+
 class Oval {
 private:
+    int width, height;
+
 public:
-	int width, height;
-public:
-	Oval() {
-		width = 1;
-		height = 1;
-	}
-	Oval(int a, int b) {
-		width = a;
-		height = b;
-	}
-	~Oval() {
-		cout << "Oval ¼Ò¸ê : width = " << width << ", height = " << height << endl;
-	}
-	int getWidth() {
-		return width;
-	}
-	int getHeight() {
-		return height;
-	}
-	void set(int w, int h) {
-		width = w;
-		height = h;
-	}
-	void show() {
-		cout << "width = " << width << ", height = " << height << endl;
-	}
+    Oval();                     
+    Oval(int a, int b);         
+    ~Oval();                    
+    int getWidth();             
+    int getHeight();            
+    void set(int w, int h);     
+    void show();                
 };
+Oval::Oval() {
+    width = 1;
+    height = 1;
+}
+
+Oval::Oval(int a, int b) {
+    width = a;
+    height = b;
+}
+
+Oval::~Oval() {
+    cout << "Oval ì†Œë©¸ : width = " << width << ", height = " << height << endl;
+}
+
+int Oval::getWidth() {
+    return width;
+}
+
+int Oval::getHeight() {
+    return height;
+}
+
+void Oval::set(int w, int h) {
+    width = w;
+    height = h;
+}
+
+void Oval::show() {
+    cout << "width = " << width << ", height = " << height << endl;
+}
+
 int main() {
 	Oval a, b(3, 4);
 	a.set(10, 20);
 	a.show();
-	cout << b.getWidth() << "," << b.getHeight() << endl;
+	cout << b.getWidth() << ", " << b.getHeight() << endl;
 
 	return 0;
-}*/
+}
