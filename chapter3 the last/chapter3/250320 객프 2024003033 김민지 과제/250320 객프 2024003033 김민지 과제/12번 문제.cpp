b440e9a648043293e2bbc39e734a66fb7a0428af
minji040421 <minji12321@naver.com>
2025-03-26 14:04:28 +0900

    Add files via upload

---
+++ chapter3 the last/chapter3/250320 객프 2024003033 김민지 과제/250320 객프 2024003033 김민지 과제/12번 문제.cpp
@@ -0,0 +1,18 @@
+#include <iostream>  
+using namespace std;
+#include<string> 
+#include<cstdlib>
+#include<ctime>
+#include "Ram.h"
+
+// main.cpp
+int main() {
+    Ram ram;
+    ram.write(100, 20); 
+    ram.write(101, 30); 
+    char res = ram.read(100) + ram.read(101); 
+    ram.write(102, res); 
+    cout << "102 번지의 값 = " << (int)ram.read(102) << endl; 
+
+    return 0;
+}
---
+++ chapter3 the last/chapter3/250320 객프 2024003033 김민지 과제/250320 객프 2024003033 김민지 과제/8번 문제.cpp
@@ -0,0 +1,42 @@
+#include <iostream>  
+using namespace std;
+#include<string> 
+#include<cstdlib>
+#include<ctime>
+
+//8¹ø ¹®Á¦
+/*
+class Integer {
+private:
+	int number;
+public:
+	Integer(int n) {
+		number = n;
+	}
+	Integer(string s) {
+		number = stoi(s);
+	}
+	int get() {
+		return number;
+	}
+	void set(int a) {
+		number = a;
+	}
+	bool isEven() {
+		return number;
+	}
+
+};
+int main() {
+	Integer n(30);
+	cout << n.get() << ' ';
+	n.set(50);
+	cout << n.get() << ' ';
+
+	Integer m("300");
+	cout << m.get() << ' ';
+	cout << m.isEven();
+
+	return 0;
+}
+*/
